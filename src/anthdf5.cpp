85b388fc877cf8079cd576e9b94fb1e3850decfc
Stuart B. Wilkins <swilkins@bnl.gov>
2020-01-14 20:41:25 -0500

    Fixed cmake for deps

--- src/anthdf5.cpp
+++ src/anthdf5.cpp
@@ -139,6 +139,22 @@ int main(int argc, char *argv[]) {
         }
     }
 
+    libconfig::Config cfg;
+
+    // Read the file. If there is an error, report it and exit.
+    try {
+        cfg.readFile("example.cfg");
+    }
+    catch(const libconfig::FileIOException &fioex) {
+        std::cerr << "I/O error while reading file." << std::endl;
+        return(EXIT_FAILURE);
+    }
+    catch(const libconfig::ParseException &pex) {
+        std::cerr << "Parse error at " << pex.getFile() << ":" << pex.getLine()
+            << " - " << pex.getError() << std::endl;
+        return(EXIT_FAILURE);
+    }
+
     exit(0);
 
     AntUsb antusb;
